1579898d9adbecc733f631dc45f405d76e5c88af
SuoSuo-Rocky <suosuo1930@hotmail.com>
2020-07-07 16:35:28 +0800

    airplane Great War is first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+ ## 版权声明：
+```txt
+
+Copyright (c) 2020 ShiWei.Co.Ltd. All rights reserved.
+
+```
---
+++ manager.py
@@ -0,0 +1,14 @@
+import traceback
+
+import pygame
+from bin.main import main
+
+if __name__ == "__main__":
+    try:
+        main()
+    except SystemExit:
+        pass
+    except:
+        traceback.print_exc()
+        pygame.quit()
+        input()
